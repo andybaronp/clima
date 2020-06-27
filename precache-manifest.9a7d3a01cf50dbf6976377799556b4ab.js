d19994062055c9d2fd03004222dfd7dacb8592e1
andy <andybaronp@gmail.com>
2020-06-27 11:46:34 -0500

    Updates

---
+++ precache-manifest.9a7d3a01cf50dbf6976377799556b4ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dc947b0eddeab1e11d8acfc036239dc",
+    "url": "/clima/index.html"
+  },
+  {
+    "revision": "03315a56eda71d4f7b51",
+    "url": "/clima/static/css/main.3e82f7ce.chunk.css"
+  },
+  {
+    "revision": "015fe4389b272b8ea129",
+    "url": "/clima/static/js/2.31e4c316.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clima/static/js/2.31e4c316.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03315a56eda71d4f7b51",
+    "url": "/clima/static/js/main.47dd2591.chunk.js"
+  },
+  {
+    "revision": "1b9741051ce960f4a1d1",
+    "url": "/clima/static/js/runtime-main.12c63bf1.js"
+  }
+]);
